eade17fa6dab1acd472435c2cb45165e13bceb1b
Chase Zheng <mzheng086@gmail.com>
2022-07-16 19:15:35 +0200

    returning svc address from module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "namespace" {
   value       = module.resources.namespace
   description = "The name (`metadata.name`) of the namespace"
 }
+
+output "svc" {
+  value = module.resources.svc
+}
